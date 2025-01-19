86449c36912b78b155e55d54ff29ecf484b65561
Владислав <vlad.shulikoff@gmail.com>
2025-01-19 15:54:09 +0700

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    //id("io.freefair.lombok") version "8.11"
 }
 
 group = "com.carshop"
@@ -15,7 +17,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.h2database:h2")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.14.0")
 }
 
 tasks.withType<Test> {
