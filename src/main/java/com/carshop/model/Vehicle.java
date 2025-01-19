b6c9a3a2b65a025059b87e441c07e9a31604736e
Владислав <vlad.shulikoff@gmail.com>
2025-01-19 15:48:21 +0700

    Created model Vehicle

---
+++ src/main/java/com/carshop/model/Vehicle.java
@@ -0,0 +1,64 @@
+package com.carshop.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "vehicles")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(length = 100)
+    private String brand;
+
+    @NotBlank
+    @Column(length = 100)
+    private String model;
+
+    @NotNull
+    @Pattern(regexp = "^(19|20)\\d{2}$", message = "Год должен быть в формате YYYY (например, 1999, 2021)")
+    private String issueYear;
+
+    @NotNull
+    private Integer price;
+
+    @NotBlank
+    @Column(length = 50)
+    private String status;
+
+    @Column(length = 50)
+    private String bodyType;
+
+    private String description;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime
+                .now()
+                .truncatedTo(ChronoUnit.SECONDS);
+    }
+}
