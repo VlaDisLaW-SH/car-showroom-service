3e37060798276c3d8962c5f720f4a50db9e74eb2
Владислав <vlad.shulikoff@gmail.com>
2025-01-19 19:15:26 +0700

    Created VehicleResponse for structured response method "index"

---
+++ src/main/java/com/carshop/dto/VehicleResponse.java
@@ -0,0 +1,17 @@
+package com.carshop.dto;
+
+import com.carshop.model.Vehicle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class VehicleResponse {
+    private List<Vehicle> vehicles;
+    private int totalElements;
+    private int totalPages;
+}
