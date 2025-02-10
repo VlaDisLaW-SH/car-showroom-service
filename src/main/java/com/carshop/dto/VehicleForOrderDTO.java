cf156fe725281e7e08b8009826320dc79a7d3c93
Владислав <vlad.shulikoff@gmail.com>
2025-02-10 18:08:46 +0700

    Added DTO of Vehicles for list Order

---
+++ src/main/java/com/carshop/dto/VehicleForOrderDTO.java
@@ -0,0 +1,15 @@
+package com.carshop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class VehicleForOrderDTO {
+    private Long id;
+    private String brand;
+    private String model;
+    private String issueYear;
+    private Integer price;
+    private String bodyType;
+}
